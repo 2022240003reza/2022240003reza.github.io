c96769655288b179c543602d242184281b61e20c
2022240003reza <76624542+2022240003reza@users.noreply.github.com>
2023-06-05 09:08:32 +0700

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<fore action="welcome.php" method="post">
-Name: <input type="text" name="post" />
-Age: <input type="text" name="age" />
-<input type="submit" />
-</fore>
-</body>
-</html>
