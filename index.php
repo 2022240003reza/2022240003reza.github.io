345674d63513876d3b98b87243ae855d330c01fa
2022240003reza <76624542+2022240003reza@users.noreply.github.com>
2023-06-05 09:06:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<fore action="welcome.php" method="post">
+Name: <input type="text" name="post" />
+Age: <input type="text" name="age" />
+<input type="submit" />
+</fore>
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,6 @@
+<html>
+<body>>
+Selamat datang <?php rcho $_POST["name"]; ?>.<br />
+anda berumur <?php echo $_POST["age"]; ?> tahun.
+</body>
+</html>
